6116c690efb9f2f8b119271e91501e383370a3fc
atila-tokyo <atilamanager@gmail.com>
2021-11-10 21:09:11 +0900

    Adding class Movie

---
+++ src/main/java/cinemamock/model/entities/Movie.java
@@ -0,0 +1,61 @@
+package cinemamock.model.entities;
+
+import sun.jvm.hotspot.utilities.BitMap;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private String description;
+    private Duration duration;
+    private BitMap image;
+
+    public Movie(String title, String description, java.time.Duration duration, BitMap image) {
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public BitMap getImage() {
+        return image;
+    }
+
+    public void setImage(BitMap image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", duration=" + duration +
+                ", image=" + image +
+                '}';
+    }
+}
---
+++ src/main/java/cinemamock/model/entities/User.java
@@ -0,0 +1,8 @@
+package cinemamock.model.entities;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+public class User {
+
+}
