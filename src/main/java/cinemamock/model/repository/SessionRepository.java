9c37c6377b90d505af1ec001c7a082a623d62f21
atila-tokyo <atilamanager@gmail.com>
2021-11-16 21:43:42 +0900

    Adding Session/Room controllers repo service files

---
+++ src/main/java/cinemamock/model/repository/SessionRepository.java
@@ -0,0 +1,10 @@
+package cinemamock.model.repository;
+
+import cinemamock.model.entities.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+}
---
+++ src/main/java/cinemamock/service/SessionService.java
@@ -0,0 +1,13 @@
+package cinemamock.service;
+
+import cinemamock.model.repository.SessionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+    private final SessionRepository sessionRepository;
+
+    public SessionService(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+}
