520c5cb07c457a32806cff74d350831d40428a43
atila-tokyo <atilamanager@gmail.com>
2021-12-15 00:17:34 +0900

    Adding Auth, websecurity and home.html, need refactor on indexController

--- src/main/java/WebSecurityConfig.java
+++ src/main/java/WebSecurityConfig.java
@@ -1,8 +1,63 @@
+import cinemamock.model.entities.User;
+import cinemamock.model.repository.UserRepository;
+import cinemamock.service.impl.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-public class WebSecurityConfig {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    private CustomUserDetailService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated().and().csrf().disable().formLogin()
+                .loginPage("/login").failureUrl("login?error=true")
+                .defaultSuccessUrl("/home")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").and().exceptionHandling()
+                .accessDeniedPage("/access-denied");
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/assets/**");
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    CommandLineRunner commandLineRunnerUser(UserRepository userRepository) {
+        return args -> {
+            User user = new User();
+            user.setEmail("hitchhiker@gmail.com");
+            user.setFirstName("Arthur");
+            user.setLastName("Dent");
+            user.setPassword(bCryptPasswordEncoder.encode("answer42"));
+            userRepository.save(user);
+        };
+    }
 }
---
+++ src/main/java/cinemamock/service/impl/CustomUserDetailService.java
@@ -0,0 +1,46 @@
+package cinemamock.service.impl;
+
+import cinemamock.model.entities.User;
+import cinemamock.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        List<GrantedAuthority> authorities = getUserAuthority();
+        return buildUserForAuthentication(user, authorities);
+    }
+
+    private List<GrantedAuthority> getUserAuthority() {
+        Set<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+        roles.add(new SimpleGrantedAuthority("user"));
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>(roles);
+        return grantedAuthorities;
+    }
+
+    private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                true, true, true, true, authorities);
+    }
+
+}
