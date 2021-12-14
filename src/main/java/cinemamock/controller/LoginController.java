23d2f343fef2198e58ba2c7f642eadd03f302ad3
atila-tokyo <atilamanager@gmail.com>
2021-12-14 17:21:12 +0900

    Adding Login Controller layer finsihed

--- src/main/java/cinemamock/controller/LoginController.java
+++ src/main/java/cinemamock/controller/LoginController.java
@@ -1,18 +1,33 @@
 package cinemamock.controller;
 
 
+import cinemamock.model.entities.User;
+import cinemamock.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+
 public class LoginController {
-    public class LoginController {
 
+    @Autowired
+    private UserService userService;
 
-
-        @GetMapping(value={"/", "/login"})
-        public String login(Model model) {
-            return login;
+    @GetMapping(value={"/", "/login"})
+    public String login(Model model) {
+            return "login";
         }
+
+    @GetMapping(value={"/home"})
+    public String home(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail((auth.getName()));
+        model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
+        return "home";
     }
+
 }
